b1d20076ffb150afd840decb47e8739c7edc486f
unknown <brendanawjang@gmail.com>
2022-01-29 15:13:47 +0800

    Add new listing route and skeleton

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,20 @@ import Home from "./Home";
 import Header from "./Header";
 import "../styles/App.css";
 import { UserProvider } from "../contexts/UserContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewListing from "./NewListing";
 
 export default function App() {
   return (
     <UserProvider>
       <div className="App">
-        <Header />
-        <Home />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-listing" element={<NewListing />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </UserProvider>
   );
---
+++ client/src/components/NewListing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NewListing() {
+  return (
+    <div>
+      <h1>New Listing</h1>
+    </div>
+  );
+}
