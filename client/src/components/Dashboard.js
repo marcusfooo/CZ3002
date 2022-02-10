9e87c10d16fc7104f3db7e8111d50456c2f6357f
unknown <brendanawjang@gmail.com>
2022-02-11 02:14:33 +0800

    Add marker on map based on postal code

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,15 +8,42 @@ import Row from "react-bootstrap/Row";
 import "../styles/Dashboard.css";
 import Map from "./Map";
 
+const getCoords = async (listing) => {
+  try {
+    const res = await fetch(
+      `https://developers.onemap.sg/commonapi/search?searchVal=${listing.postalCode}&returnGeom=Y&getAddrDetails=Y&pageNum=1`
+    );
+    const results = await res.json();
+    if (results.results.length > 0) {
+      return [
+        parseFloat(results.results[0]["LATITUDE"]),
+        parseFloat(results.results[0]["LONGITUDE"]),
+      ];
+    } else {
+      return;
+    }
+  } catch (err) {
+    console.error("Failed to fetch coordinates");
+  }
+};
+
 export default function Dashboard() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [listings, setListings] = useState([]);
+  const [coords, setCoords] = useState([]);
 
   useEffect(() => {
     async function getListings() {
       const res = await axios.get("/api/listing", searchParams);
       console.log(res);
       setListings(res.data.listings);
+      const coords = await Promise.all(
+        res.data.listings.map(async (listing) => {
+          const res = await getCoords(listing);
+          return res;
+        })
+      );
+      setCoords(coords.filter(Boolean));
     }
     getListings();
   }, [searchParams]);
@@ -26,14 +53,14 @@ export default function Dashboard() {
       <Row className="filter-bar">
         <h5>Filters</h5>
       </Row>
-      <Row>
+      <Row className="h-100">
         <Col sm={7} className="list">
           {listings.map((listing, idx) => (
             <ListingCard {...listing} key={idx} />
           ))}
         </Col>
         <Col className="p-0">
-          <Map />
+          <Map coords={coords} />
         </Col>
       </Row>
     </Container>
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-export default function SGMap() {
+export default function SGMap({ coords }) {
   return (
     <MapContainer
       center={[1.360278, 103.8083]}
@@ -17,11 +16,13 @@ export default function SGMap() {
         attribution='<img src="https://docs.onemap.gov.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
         url="https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png"
       />
-      <Marker position={[1.360278, 103.8083]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {coords.map((coord) => (
+        <Marker position={coord}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
