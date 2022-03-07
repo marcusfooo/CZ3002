e4f1f2bdc9a2aae51a9f61225c0aeca72ac74cc9
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-03-07 20:31:02 +0800

    Temporarily set prod paths for testing

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let development = process.env.NODE_ENV !== "production";
 
 const instance = axios.create({
-  baseURL: development ? "http://www.rentsg.software:5000" : process.env.PRODUCTION_URL,
+  baseURL: "http://www.rentsg.software:5000" ,
   // baseURL: development ? "http://localhost:5000" : process.env.PRODUCTION_URL,
 });
 
