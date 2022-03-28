e59115ebc331d30b8b2d878e1b4d8d82375e42c3
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-03-28 16:25:29 +0800

    .env

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 let development = process.env.NODE_ENV !== "production";
 
 const instance = axios.create({
-  baseURL: "http://www.rentsg.software:5000" ,
-  // baseURL: development ? "http://localhost:5000" : process.env.PRODUCTION_URL,
+  // baseURL: "http://www.rentsg.software:5000" ,
+  baseURL: development ? "http://localhost:5000" : process.env.PRODUCTION_URL,
 });
 
 export default instance;
