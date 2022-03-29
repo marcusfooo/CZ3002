615620c547977392862a8351333ad133eadc2c3f
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-03-29 11:54:26 +0800

    Fixed links

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 let development = process.env.NODE_ENV !== "production";
 
 const instance = axios.create({
-  baseURL: "http://localhost" ,
-  // baseURL: "http://www.rentsg.software" ,
+  // baseURL: "http://localhost" ,
+  baseURL: "http://www.rentsg.software" ,
   // baseURL: development ? "http://localhost:5000" : process.env.PRODUCTION_URL,
 });
 
