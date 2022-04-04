e8ed17da7f6de4d0a92a1baf94d5dca9df077a0b
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-04-04 12:17:05 +0800

    Added /api to axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,7 +4,7 @@ let development = process.env.NODE_ENV !== "production";
 const instance = axios.create({
   // baseURL: "http://localhost" ,
   // baseURL: "http://www.rentsg.software" ,
-  baseURL: development ? "http://localhost:5000" : "http://www.rentsg.software",
+  baseURL: development ? "http://localhost:5000/api" : "http://www.rentsg.software/api",
 });
 
 export default instance;
