f2db16cf481dcccaa55b2cab25fe3b2319c14ae7
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-10-12 14:17:49 +0800

    edited compose

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 let development = process.env.NODE_ENV !== "production";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/" ,
+  // baseURL: "http://localhost:5000/" ,
   // baseURL: "http://www.rentsg.software" ,
-  // baseURL: development ? "http://localhost:5000/api" : "http://www.rentsg.software/api",
+  baseURL: development ? "http://localhost:5000/" : "http://www.rentsg.software/api",
 });
 
 export default instance;
