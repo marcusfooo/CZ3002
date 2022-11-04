d3ca97f5eb10c4529a0600417cee82928f26766a
syakun0212 <shaoyk0212@gmail.com>
2022-11-04 23:18:25 +0800

    update auth test

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,56 +1,137 @@
-# # import sys
-# # sys.path.insert(0, '../models/')
-# # import user
-
-# from test_client import create_app
-# # from api import db
-# import sys
-# sys.path.insert(0, '../')
-# from models.user import User
+import pytest, os 
+from api import db 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_user, login_required, logout_user
+from itsdangerous import SignatureExpired, URLSafeTimedSerializer
+import time 
 
 
-# def test_signup_post_success():
-#     app = create_app()
-#     client = app.test_client()
-#     url = '/api/signup'
-    
-#     email = 'shaoyk0212@gmail.com'
-#     password = '123456789'
-#     new_user = User(email=email, password=password)
-    
-#     response = client.post(url,
-#                           new_user)
-    
-#     assert response.status_code == 200
-import pytest 
+import sys
+# sys.path.insert(0, '../api')
+# import pprint
+# pprint.pprint(sys.path)
+
+from test_client import create_app
+from user import User
+from ..auth import login_post, logout, confirm_email, send_confirm_email
+
+
+auth_s = URLSafeTimedSerializer("secret key")
+
 
 class TestAuth:
+    
+    @pytest.fixture(scope='session', autouse=True)
+    def app(self):
+        print("running test_auth.py")
+
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+        with self.app.app_context():
+            db.create_all()
+            yield self.app
+            db.session.remove()
+            db.drop_all()       
+
+        
     def test_login_post_success(self):
-        return 0
+        email = 'shaoyk0212@gmail.com'
+        password = '123456789'
+        user = User.query.filter_by(email=email).first()
+
+        assert user 
+        assert check_password_hash(user.password, password)
+        
+        login_user(user)
+        assert current_user.username == email
+        # pass
+        
     
     def test_login_post_wrong_password(self):
-        return 0
+        email = 'shaoyk0212@gmail.com'
+        password = 'abddfsfdfsx'
+        
+        user = User.query.filter_by(email=email).first()
+        assert check_password_hash(user.password, password) == False
+        # pass
+        
     
     def test_login_post_email_unverified(self):
-        return 0
+        email = 'shaoyk0212@gmail.com'
+        password = 'abddfsfdfsx'
+        
+        user = User.query.filter_by(email=email).first()     
+        assert user.isVerified == False
+        # pass
+        
     
     def test_signup_post_success(self):
-        return 0
+        url = '/api/singup'
+        email = 'shao0041@e.ntu.edu.sg'
+        password = '123456789'
+        
+        user = User.query.filter_by(email=email).first()
+        
+        password = generate_password_hash(password, method='sha256')
+        new_user = User(email=email, password=password)
+        
+        assert user == False   
+        # pass
     
     def test_signup_post_invalid_pw(self):
-        return 0
+        url = '/api/singup'
+        email = 'shao0041@e.ntu.edu.sg'
+        password = '123459'
+        
+        user = User.query.filter_by(email=email).first()
+        
+        password = generate_password_hash(password, method='sha256')
+        new_user = User(email=email, password=password)
+        
+        assert len(password) < 8
+        # pass
+    
     
     def test_signup_post_email_exists(self):
-        return 0
+        email = 'shaoyk0212@gmail.com'
+        password = 'abddfsfdfsx'
+        
+        user = User.query.filter_by(email=email).first()
+        assert user == True 
+        # pass
+        
+        
     
     def test_logout(self):
-        return 0
+        email = 'shaoyk0212@gmail.com'
+        password = '123456789'
+        user = User.query.filter_by(email=email).first()
+        
+        login_post(user)
+        
+        response = logout()
+        assert response.status_code == 200
+        # pass
 
     def test_confirm_email_success(self):
-        return 0
+        email = 'shao0041@e.ntu.edu.sg'
+        token = auth_s.dumps(email, salt='email-confirm')
+        
+        response = confirm_email(token)
+        assert response.status_code == 200
+        
+        
 
     def test_confirm_email_expired(self):
-        return 0
+        email = 'shao0041@e.ntu.edu.sg'
+        token = auth_s.dumps(email, salt='email-confirm', expires_in=0.0001)
+        time.sleep(0.0002)
+        response = confirm_email(token)
+        assert response.status_code == 401
+        # pass
 
     def test_send_confirm_email(self):
-        return 0
+        response = send_confirm_email()
+        assert response.status_code ==200
+        # pass
